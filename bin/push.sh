099afef38639113ac88d272e1a843f9ab921e793
YuhongCh <yuhchen@umich.edu>
2021-11-23 13:07:29 -0500

    index blog html

--- bin/push.sh
+++ bin/push.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 
-# transform all md file in source to html
-for md_file in source/*
-do
-    filename=$(basename $md_file .md)
-    pandoc --standalone --fail-if-warning --template build/template.html $md_file -o "$filename.html"
-done
-
-# push to github
+# transform all md file in source to htmlub
 git add .
 git commit -m "shell auto-submit"
 git push origin main
